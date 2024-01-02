86cff4481a798b68d47e36615ab7cefc12f484fc
Virtuso1225 <dominico1225@gmail.com>
2024-01-02 16:50:14 +0900

    DevKor project init

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'prettier'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'eslint-plugin-tsdoc', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-console': ['warn'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-dupe-class-members': ['error'],
+    '@typescript-eslint/no-useless-constructor': ['error'],
+    '@typescript-eslint/no-inferrable-types': ['off'],
+    '@typescript-eslint/no-empty-interface': ['off'],
+    '@typescript-eslint/no-explicit-any': ['warn'],
+    '@typescript-eslint/consistent-type-imports': ['warn']
+  }
+}
